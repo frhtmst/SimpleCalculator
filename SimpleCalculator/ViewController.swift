cb9d19107f56919d954d3c6278559a61e977ce49
Ferhat Mesut Görür <ferhatmesut@gmail.com>
2022-09-20 19:59:50 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Ferhat Mesut Görür on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
